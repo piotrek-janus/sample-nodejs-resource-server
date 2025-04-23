666660876021bde4d3ce936057cef9e140e023b2
Piotr Janus <pjanus@secureauth.com>
2025-04-23 15:28:20 +0200

    add comments and change validation

--- src/oauth.js
+++ src/oauth.js
@@ -32,9 +32,9 @@ export const createOAuthJWTValidator = async function(config) {
         }
     } 
 
+    // this jwks is pulled from the well-known endpoint and by default cached for 30s
+    // see docs https://github.com/panva/jose/blob/2b42c5872b92a2c5662b26facd910b6d8e95f008/docs/jwks/remote/functions/createRemoteJWKSet.md
     const jwks = jose.createRemoteJWKSet(new URL(config.jwks_uri))
-
-    console.log(jwks)
 
     return function(scope) {
         return async function (req, res, next) {
@@ -46,18 +46,22 @@ export const createOAuthJWTValidator = async function(config) {
             token = token.replace('Bearer ', '')
             
             try {
+                // this function will validate the token locally
                 const { payload } = await jose.jwtVerify(token, jwks, {
                     issuer: config.issuer,
                     audience: config.audience,
                   })
                     
+                // expect the token to contain the required scope
                 if (scope && !payload.scp.includes(scope)) {
                     return unauthorized(res, ErrMissingScope)
                 }
 
                 req.token = payload
             } catch (err) {
-                if (err.code === 'ERR_JWS_VERIFICATION_FAILED') {
+                if (err instanceof jose.errors.JWSSignatureVerificationFailed || 
+                    err instanceof jose.errors.JWTInvalid || 
+                    err instanceof jose.errors.JWKSNoMatchingKey) {   
                     return unauthenticated(res, err.message)
                 } else {
                     return unauthorized(res, err.message)
@@ -65,6 +69,8 @@ export const createOAuthJWTValidator = async function(config) {
             }
 
             try {    
+                // results of userinfo endpoint can be cached for some time (~30s?) per token, 
+                // to avoid calling the endpoint for each request to the resource server
                 await fetch(config.userinfo_endpoint, {
                     headers: {
                         "Authorization": `Bearer ${token}`
